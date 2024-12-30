912af2fd4c2bd3cf174ae74ab95a436c4385ca56
Jo <j.gugerbauer@gmail.com>
2024-12-30 14:38:43 +0100

    add some functions

--- src/Vector2D/vector2d.ts
+++ src/Vector2D/vector2d.ts
@@ -10,10 +10,14 @@ export class Vector2D {
     public toString() {
         return `x: ${this.x}, y: ${this.y}`;
     }
-    public static add(v1: Vector2D, v2: Vector2D): Vector2D {
-        return { x: v1.x + v2.x, y: v1.y + v2.y }
+    public static add(...vectors: Vector2D[]): Vector2D {
+        return vectors.reduce((previous, current) => {
+            return {x: previous.x + current.x, y: previous.y + current.y};
+        }, {x: 0, y: 0});
     }
-    
+    /**
+     * @returns v1 - v2
+     */
     public static subtract(v1: Vector2D, v2: Vector2D): Vector2D {
         return { x: v1.x - v2.x, y: v1.y - v2.y }
     }
@@ -33,5 +37,21 @@ export class Vector2D {
         const mag = this.magnitude(v);
         return mag === 0 ? { x: 0, y: 0 } : this.scale(v, 1 / mag);
     }
-    
+    /**
+     * @param v1 Vector2D
+     * @param v2 Vector2D
+     * @returns Distance from v1 to v2
+     */
+    public static distance(v1: Vector2D, v2: Vector2D): number {
+        return Math.sqrt((v2.x - v1.x)**2 + (v2.y - v1.y)**2);
+    }
+    /**
+     * returns a vector pointing from v1 to v2
+     */
+    public static displacementVector(v1: Vector2D, v2: Vector2D): Vector2D {
+        return this.subtract(v2, v1);
+    }
+    public static hadamardProduct(v1: Vector2D, v2: Vector2D): Vector2D {
+        return {x: v1.x * v2.x, y: v1.y * v2.y};
+    }
 }
